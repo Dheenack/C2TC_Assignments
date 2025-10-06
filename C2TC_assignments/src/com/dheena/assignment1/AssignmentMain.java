0e1509fbfee46a65338827a604b39daba7b9faa4
Dheena Krishna <dheenakrishna2020@gmail.com>
2025-10-06 10:44:11 +0530

    minor change

--- C2TC_assignments/src/com/dheena/assignment1/AssignmentMain.java
+++ C2TC_assignments/src/com/dheena/assignment1/AssignmentMain.java
@@ -5,7 +5,7 @@ import com.dheena.assignment1.employees.Manager;
 import com.dheena.assignment1.utilities.EmployeeUtilities;
 
 public class AssignmentMain {
-
+// main method
 	public static void main(String[] args) {
 		 Manager manager = new Manager("muthamizh", 202, 234517, "HR");
 	        
